14defdcef74c8aee571d6f71144565ac918c851a
LuanRaabe <luan.raabe202@gmail.com>
2022-08-01 04:10:52 -0300

    telas de saque, deposito e transferencia

--- frontend/src/pages/Deposit/index.tsx
+++ frontend/src/pages/Deposit/index.tsx
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { UploadSimple } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
 import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { SmallInput } from '../../components/Form/Panel/SmallInput';
 import { Modal } from '../../components/Modal';
-import { api } from '../../libs/api';
+import { WhiteCard } from '../../components/WhiteCard';
+import { useUser } from '../../providers/UserProvider';
+import { maskValue } from '../../utils/Masks';
+import { InputReferences } from '../../utils/References';
+import { validateValue, validatePassword } from '../../utils/Validators';
+
+const TAB_INDEX = {
+  VALUE: 1,
+  PASSWORD: 2,
+  BUTTON: 3,
+};
 
 /**
- * Archive: src/pages/Deposit.tsx
+ * Archive: src/pages/Transfer.tsx
  *
- * Description: Deposit page
+ * Description: Transfer page
  *
- * Date: 2022/07/20
+ * Date: 2022/07/30
  *
- * Author: Rey
+ * Author: Rey, Luan
  */
 
-export const Deposit = () => {
+export const Transfer = () => {
   const [modal, setModal] = useState(false);
+  const [disableSubmit, setDisableSubmit] = useState<boolean>(false);
+  const references = InputReferences();
+  const { loggedAccount } = useUser();
+  const [amount, setAmount] = useState<string>('0.00');
+  const [password, setPassword] = useState<string>('');
 
-  async function handleDeposit() {
-    try {
-      const result = await api.post('deposit', {
-        agency: '',
-        account: '',
-        value: '',
-      });
-    } catch (error) {
-      console.log(error);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    setModal(true);
+  }
+
+  function confirSubmit() {
+    /* const response = request;
+    const {data, error} = response;
+    if(error){
+      const {name, message} = error;
+      references.setError(name, message);
+      return;
     }
+    navigate('/transaction', {state: {transactionId: });
+    */
   }
 
   return (
@@ -34,26 +57,64 @@ export const Deposit = () => {
         <Modal
           title="Depósito"
           setModal={setModal}
-          handleConfirmModal={handleDeposit}
+          handleConfirmModal={confirSubmit}
         />
       )}
-      <div className="flex flex-col gap-5">
-        <Button
-          category="primary"
-          label="Abrir modal"
-          onClick={() => setModal(true)}
-        />
-        <Button
-          category="secondary"
-          label="Abrir modal"
-          onClick={() => setModal(true)}
-        />
-        <Button
-          category="cancel"
-          label="Abrir modal"
-          onClick={() => setModal(true)}
-        />
-      </div>
+      <WhiteCard
+        icon={<UploadSimple className="w-5 h-5" />}
+        title="Depósito"
+        blank={true}
+      >
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <span className="text-base font-normal">Dados para depósito</span>
+          <div className="flex flex-row justify-between mb-4">
+            <SmallInput
+              title="Agência"
+              isDisabled={true}
+              value={loggedAccount?.agency + '-' + loggedAccount?.agencyDigit}
+            />
+            <SmallInput
+              title="Conta"
+              isDisabled={true}
+              value={loggedAccount?.account + '-' + loggedAccount?.accountDigit}
+            />
+          </div>
+          <Input
+            name="value"
+            placeholder="Valor"
+            type="text"
+            value={amount}
+            onChange={setAmount}
+            mask={maskValue}
+            validators={[
+              { validate: validateValue, errorMessage: 'Valor inválido' },
+            ]}
+            ref={references.getOrCrateRef('value')}
+            callback={setDisableSubmit}
+            tabIndex={TAB_INDEX.VALUE}
+          />
+          <Input
+            name="password"
+            placeholder="Senha"
+            type="text"
+            value={password}
+            onChange={setPassword}
+            validators={[
+              { validate: validatePassword, errorMessage: 'Senha inválida' },
+            ]}
+            ref={references.getOrCrateRef('password')}
+            callback={setDisableSubmit}
+            tabIndex={TAB_INDEX.PASSWORD}
+          />
+          <Button
+            category="primary"
+            label="Depositar"
+            type="submit"
+            isDisabled={disableSubmit}
+            tabIndex={TAB_INDEX.BUTTON}
+          />
+        </form>
+      </WhiteCard>
     </>
   );
 };
