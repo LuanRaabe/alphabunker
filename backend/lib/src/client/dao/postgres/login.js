24b97572fd19319d411e6504e2aab2540d28f481
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-08-02 01:23:25 -0300

    mudança do login

--- backend/lib/src/client/dao/postgres/login.js
+++ backend/lib/src/client/dao/postgres/login.js
@@ -50,16 +50,20 @@ function LoginDB(cpf, password) {
             }
             if (account) {
                 return {
-                    id: account.id,
-                    name: owner.name,
-                    email: owner.email,
-                    birthdate: owner.birthdate,
-                    owners_cpf: account.cpf,
-                    agency: account.agency,
-                    agency_digit: account.agency_digit,
-                    account: account.account,
-                    account_digit: account.account_digit,
-                    balance: account.balance.toFixed(2),
+                    owner: {
+                        name: owner.name,
+                        email: owner.email,
+                        birthdate: owner.birthdate,
+                    },
+                    account: {
+                        id: account.id,
+                        owners_cpf: account.cpf,
+                        agency: account.agency,
+                        agency_digit: account.agency_digit,
+                        account: account.account,
+                        account_digit: account.account_digit,
+                        balance: account.balance.toFixed(2),
+                    }
                 };
             }
             return false;
